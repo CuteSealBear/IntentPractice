963adcf820681d4c0e02ceca67927fee0195f525
CuteSealBear <CuteSealBear@gmail.com>
2022-06-02 14:19:36 +0900

    결과를 받아 돌아온 화면에서 새 닉네임 반영 -에러발생

--- app/src/main/java/com/cutesealbear/intentpractice/MainActivity.kt
+++ app/src/main/java/com/cutesealbear/intentpractice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cutesealbear.intentpractice
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -49,4 +50,24 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+//        돌아온 이유가 닉네임을 받으러 온 것이 맞는지?
+        if (requestCode == REQUEST_FOR_NICKNAME) {
+
+//            추가질문 : 확인을 눌러서 돌아온 것이 맞는가?
+
+            if (requestCode == RESULT_OK) {
+
+//                실제 첨부된 새 닉네임을 꺼내서 텍스트뷰에 반영
+                val newNickname = data?.getStringExtra("nickname")
+                nicknameTxt.text = newNickname
+            }
+
+        }
+
+    }
+
 }
